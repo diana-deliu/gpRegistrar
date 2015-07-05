de27801debcdeb607e6879e485a4ad742b684ba2
Diahne <deliudiana@gmail.com>
2015-07-05 13:39:13 +0300

    Tweaking addmedic

--- resources/views/admin/addmedic.blade.php
+++ resources/views/admin/addmedic.blade.php
@@ -13,14 +13,14 @@
             <div class="form-group">
                 <label for="lastname" class="col-lg-2 col-xs-offset-1 col-md-offset-1 col-lg-offset-0 control-label">Nume</label>
 
-                <div class="col-lg-2 col-xs-offset-1 col-md-offset-1 col-lg-offset-0 control-label">
+                <div class="col-lg-5 col-xs-10 col-xs-offset-1 col-md-offset-1 col-lg-offset-0 control-label">
                     {!! Form::text('lastname', old('lastname'), ['class' => 'form-control'])!!}
                 </div>
             </div>
             <div class="form-group">
                 <label for="firstname" class="col-lg-2 col-xs-offset-1 col-md-offset-1 col-lg-offset-0 control-label">Prenume</label>
 
-                <div class="col-lg-2 col-xs-offset-1 col-md-offset-1 col-lg-offset-0 control-label">
+                <div class="col-lg-5 col-xs-10 col-xs-offset-1 col-md-offset-1 col-lg-offset-0 control-label">
                     {!! Form::text('firstname', old('firstname'), ['class' => 'form-control'])!!}
                 </div>
             </div>
